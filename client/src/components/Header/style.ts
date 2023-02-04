6db34388219999fe9fb2e3b7ef79d880036bd67e
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-02-04 16:51:50 -0300

    feat: Header component and its style created

---
+++ client/src/components/Header/style.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 66,
+    backgroundColor: "#22548E",
+
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+
+    marginTop: 25,
+    padding: 20,
+  },
+  content: {
+    left: 120,
+  },
+  viewTexts: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 15,
+    lineHeight: 19,
+    color: "#FFFFFF",
+  },
+});
+
+export { styles };
